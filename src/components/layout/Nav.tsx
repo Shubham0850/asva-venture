4eddd32d70660bc5ba0d883ac23765af080b7f1e
Kishor <yoyokunal156@gmail.com>
2023-07-13 16:18:09 +0530

    navbar

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,8 +1,63 @@
-import { Container } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  IconButton,
+  Link,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 import { useState } from "react";
 
 function NavBar() {
-  return <Container maxW={1200}>Nav</Container>;
+  return (
+    <Container maxW={1300}>
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        py={4}
+        px={10}
+        bg="transparent"
+        color="white"
+      >
+        {/* Logo */}
+        <Flex>
+          <img src="/dark-logo.png" alt="asva logo" />
+        </Flex>
+
+        {/* Links */}
+        <Flex
+          gap={20}
+          display={{ base: "none", md: "flex" }}
+          alignItems="center"
+        >
+          <Link
+            href="/"
+            mr={4}
+            color="black"
+            _hover={{
+              color: "black",
+              textDecoration: "underline",
+              fontWeight: "bold",
+            }}
+          >
+            Home
+          </Link>
+          <Link href="#" mr={4} color="black">
+            Our Thesis
+          </Link>
+          <Link href="#" mr={4} color="black">
+            Blog
+          </Link>
+          <Link href="#" mr={4} color="black">
+            Contact
+          </Link>
+        </Flex>
+      </Flex>
+    </Container>
+  );
 }
 
 export default NavBar;
