34c64eff2cc3ae89ffe3613eea1edd3a36dae1e0
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-13 11:29:40 +0530

    hero section

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,7 +1,31 @@
 import { Container } from "@chakra-ui/react";
+import React from "react";
+import { Box, ChakraProvider, Button, Text } from "@chakra-ui/react";
 
 function Hero() {
-  return <Container maxW={1300}>heo</Container>;
+  return (
+    <Container maxW={1300}>
+      <ChakraProvider>
+        <Box p={4}>
+          <Box mb={4}>
+            <Text>
+              We are backing visionary founders Build on next-generation
+              internet
+            </Text>
+          </Box>
+          <Box mb={4}>
+            <Text>
+              A thesis-driven blockchain-focused venture fund strategically
+              investing in pre-seed and seed companies we lead, co-investments
+              and bring our invaluable expertise to our portfolio projects to
+              grow beyond.
+            </Text>
+          </Box>
+          <Button colorScheme="blue">Click me</Button>
+        </Box>
+      </ChakraProvider>
+    </Container>
+  );
 }
 
 export default Hero;
