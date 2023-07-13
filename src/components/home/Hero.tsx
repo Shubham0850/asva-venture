05533527eec593c3a750f3370124e216ead9f9ee
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-13 17:56:44 +0530

    complete hero section texts

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,28 +1,51 @@
 import { Container } from "@chakra-ui/react";
 import React from "react";
-import { Box, ChakraProvider, Button, Text, } from "@chakra-ui/react";
+import {BsArrowRight} from "react-icons/bs"
+import { Box, ChakraProvider, Button, Text,Icon } from "@chakra-ui/react";
 
 function Hero() {
   return (
-    <Container maxW={1300}>
+    <Container maxW={1300} >
       <ChakraProvider>
-        <Box p={4}>
-          <Box mb={4} fontWeight={700} width={'930px'}>
-            <Text  >
-              We are backing <b color="#80FF7D">visionary</b>  founders Build on next-generation
-              internet
-            </Text>
-          </Box>
-          <Box mb={4}>
-            <Text>
-              A thesis-driven blockchain-focused venture fund strategically
-              investing in pre-seed and seed companies we lead, co-investments
-              and bring our invaluable expertise to our portfolio projects to
-              grow beyond.
-            </Text>
-          </Box>
-          <Button backgroundColor="#80FF7D" color={'#111111'}   padding={'20px, 24px, 20px, 24px'} >Talk with Us</Button>
+      <Box p={4} paddingTop={['90px', '40px']}>
+        <Box mb={6}>
+          <Text
+          width={['100%', '930px']}
+          fontSize={['26px', '48px']}
+            style={{           
+              height: '130px',
+              fontWeight: 700,
+            }}
+          >
+            We are backing{' '}
+            <span style={{ color: '#80FF7D' }}>visionary</span> founders Build on next-generation
+            internet
+          </Text>
         </Box>
+        <Box mb={6}>
+          <Text
+           width={['100%', '930px']}
+           fontSize={['12px', '20px']}
+            style={{
+              height: '92px',
+              fontWeight: 400,
+            }}
+          >
+            A thesis-driven blockchain-focused venture fund strategically investing in pre-seed and
+            seed companies we lead, co-investments and bring our invaluable expertise to our
+            portfolio projects to grow beyond.
+          </Text>
+        </Box>
+        <Button
+          backgroundColor="#80FF7D"
+          color="#111111"
+          padding={['16px', '20px 24px']}
+          fontSize={['12px', '16px']}
+        >
+          Talk with Us
+          <Icon className="blink" as={BsArrowRight} color={"#111111"} m={2} />
+        </Button>
+      </Box>
       </ChakraProvider>
     </Container>
   );
