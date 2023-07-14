cf0f296286b2d7aa611fb425fbd50027272661d4
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-14 17:24:01 +0530

    add data

---
+++ src/data/recentResearchData.js
@@ -0,0 +1,32 @@
+export const RecentResearchData = [
+  
+  { 
+    category:'Policy & Regulation',
+    link:'#',
+    title:'Building your API stack',
+    description:'How do you create compelling presentations that wow your colleagues and impress your managers?',
+    writer:'PHOENIX BAKER',
+    date:'19 Jan 2024',
+   
+  },
+  { 
+        category:'Research',
+        link:'#',
+        title:'Building your API stack',
+        description:'How do you create compelling presentations that wow your colleagues and impress your managers?',
+        writer:'PHOENIX BAKER',
+        date:'19 Jan 2024',
+       
+      },
+      { 
+        category:'Tech Trends',
+        link:'#',
+        title:'Building your API stack',
+        description:'How do you create compelling presentations that wow your colleagues and impress your managers?',
+        writer:'PHOENIX BAKER',
+        date:'19 Jan 2024',
+       
+      },
+      
+
+];
