13ec6abcdca729a062acbe0769eb22c3aac0955e
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-14 16:22:55 +0530

    completed

--- src/components/home/RecentPosts.tsx
+++ src/components/home/RecentPosts.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Box, Container, Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+import { RecentPostData } from "../../data/recentPostData";
+import RecentCard from "../Cards/RecentCard";
 
 function RecentPosts() {
   return (
-	<div>RecentPosts</div>
-  )
+    <Container maxW={1300}>
+      <Flex justifyContent={"space-between"} p={5}>
+        <Text>Recent post</Text>
+
+        <Link href={"#"}>See all</Link>
+      </Flex>
+
+      {/* Render cards component */}
+      <Flex alignItems={"center"} wrap={"wrap"}>
+        {RecentPostData.map((postData) => {
+          const { bannerImg, title, date, description, postLink, writer } =
+            postData;
+          return (
+            <RecentCard
+              writer={writer}
+              date={date}
+              bannerImg={bannerImg}
+              description={description}
+              title={title}
+              postLink={postLink}
+            />
+          );
+        })}
+      </Flex>
+    </Container>
+  );
 }
 
-export default RecentPosts
+export default RecentPosts;
