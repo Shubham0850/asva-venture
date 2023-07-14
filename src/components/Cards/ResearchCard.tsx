c6aaf028cbe5b60caa2fcda1af39de2ef157a069
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-14 17:24:40 +0530

    complete

--- src/components/Cards/ResearchCard.tsx
+++ src/components/Cards/ResearchCard.tsx
@@ -1,8 +1,27 @@
+import { Box,Text ,Link,Flex} from '@chakra-ui/react'
 import React from 'react'
 
-function ResearchCard() {
+
+interface  RecentResearchPropsType {
+  category:string,title:string,link:string,description:string,date:string,writer:string
+}
+
+function ResearchCard({category,title,link,description,date,writer} : RecentResearchPropsType) {
   return (
-	<div>ResearchCard</div>
+<Box width={'30%'} p={10} border={'2px'} >
+  <Flex justifyContent={'space-between'}> 
+    <Text>{category}</Text>
+    <Link href={link}>icon</Link>
+  </Flex>
+    
+   <Text>{title}</Text>
+   <Text>{description}</Text>
+    
+    <Flex>
+      <Text>{writer}</Text>
+      <Text>{date}</Text>
+    </Flex>
+</Box>
   )
 }
 
