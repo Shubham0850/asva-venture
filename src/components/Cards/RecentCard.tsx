d9bc34a152e0b489e1debf36c1976ce53cd12745
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-14 16:22:30 +0530

    make card

--- src/components/Cards/RecentCard.tsx
+++ src/components/Cards/RecentCard.tsx
@@ -1,8 +1,28 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
-function RecentCard() {
+interface RecenCardPropsType {
+  bannerImg: string,
+  description: string,
+  title: string,
+  writer: string,
+  date: string,
+  postLink: string
+}
+
+function RecentCard({bannerImg, description, title, writer,date, postLink}: RecenCardPropsType) {
   return (
-	<div>RecentCard</div>
+	<Box width={"50%"} p={5}>
+    <Image src={bannerImg} width={"100%"} alt="banner" />
+    <Text>{title}</Text>
+    <Text>{description}</Text>
+    <Flex>
+      <Text>{writer}</Text>
+      <Text>{date}</Text>
+    </Flex>
+
+
+  </Box>
   )
 }
 
