5ae8847d8bfd50ec7f50ac6e3076bc79f282b487
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-14 16:21:04 +0530

    data details

---
+++ src/data/recentPostData.js
@@ -0,0 +1,42 @@
+export const RecentPostData = [
+  {
+    bannerImg:
+      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/higher-education-banner-post-template-design-2d8307ca851667098be33beb6faa660f_screen.jpg?ts=1621844342",
+    title: "Migrating to Linear 101",
+    description:
+      "How do you create compelling presentations that wow your colleagues and impress your managers?",
+    writer: "Jayjeet kumar",
+    date: "19 July 2022",
+    postLink: "#",
+  },
+  {
+    bannerImg:
+      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/higher-education-banner-post-template-design-2d8307ca851667098be33beb6faa660f_screen.jpg?ts=1621844342",
+    title: "Migrating to Linear 101",
+    description:
+      "How do you create compelling presentations that wow your colleagues and impress your managers?",
+    writer: "Jayjeet kumar",
+    date: "19 July 2022",
+    postLink: "#",
+  },
+  {
+    bannerImg:
+      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/higher-education-banner-post-template-design-2d8307ca851667098be33beb6faa660f_screen.jpg?ts=1621844342",
+    title: "Migrating to Linear 101",
+    description:
+      "How do you create compelling presentations that wow your colleagues and impress your managers?",
+    writer: "Jayjeet kumar",
+    date: "19 July 2022",
+    postLink: "#",
+  },
+  {
+    bannerImg:
+      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/higher-education-banner-post-template-design-2d8307ca851667098be33beb6faa660f_screen.jpg?ts=1621844342",
+    title: "Migrating to Linear 101",
+    description:
+      "How do you create compelling presentations that wow your colleagues and impress your managers?",
+    writer: "Jayjeet kumar",
+    date: "19 July 2022",
+    postLink: "#",
+  },
+];
