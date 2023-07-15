ec09cfc28bafad6b0a190e2896d282732fe42272
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-15 15:45:02 +0530

    styling

--- src/components/Cards/AnnouncementCard.tsx
+++ src/components/Cards/AnnouncementCard.tsx
@@ -1,5 +1,6 @@
 import { Box,Text ,Link,Flex} from '@chakra-ui/react'
 import React from 'react'
+import { HiOutlineArrowUpRight } from "react-icons/hi2";
 
 interface announcementprops{
   title:string,
@@ -11,21 +12,57 @@ interface announcementprops{
 
 function AnnouncementCard({title,name,date,tags,link}: announcementprops) {
   return (
-    <Box width={'30%'} border={'2px'}>
-       <Flex justifyContent={'space-between'}>
-        <Text>{title}</Text>
-        <Link href={link}>icon</Link>
-       </Flex>
+    <Box 
+    border={"1px solid #D9D9D9"}
+    padding={"32px 24px"}
+    position={"relative"}
+    >
+
+    
+        <Text  
+        width={'296px'}
+        height={'64px'}
+         color={"#4C545A"}
+        fontSize={24}
+        fontFamily={"PowerGrotesk"}
+        fontWeight={500}
+        mb={2}
+        >{title}
+        </Text>
+    
+ 
          
-         <Flex>
-          <Text>{tags}</Text>
+         <Flex >
+          <Text color={"#8C9295"}
+        fontSize={16}
+        fontFamily={"PowerGrotesk"}
+        fontWeight={400}
+        mb={"70px"}
+        >{tags}</Text>
 
          </Flex>
 
-         <Flex>
-          <Text>{name}</Text>
-          <Text>{date}</Text>
-         </Flex>
+         <Flex fontSize={14}>
+        <Text
+          textTransform={"uppercase"}
+          color={"#798D9B"}
+          fontWeight={800}
+          mr={1}
+        >
+          {name} -{" "}
+        </Text>
+        <Text fontWeight={400} color={"#798D9B"}>
+          {date}
+        </Text>
+      </Flex>
+
+         <Box position={"absolute"} top={"12px"} right={"12px"}>
+        <Link href={"#"}>
+          <HiOutlineArrowUpRight
+            style={{ fontSize: 20, color: "#4C545A", marginLeft: 5 }}
+          />
+        </Link>
+      </Box>
     </Box>
 
   );
