12036c7d1a35a4f23d948c87b5d497b239d1328a
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-15 15:59:32 +0530

    change some data

--- src/data/announcementData.js
+++ src/data/announcementData.js
@@ -2,21 +2,21 @@ export const AnnouncementData = [
         {
                 title:'Podcast: Creating a better CX Community',
                 link:'#',
-                tags:['tag1','tag2'],
+                tags:['Policy & Regulation','Code & Engineering'],
                 name:'PHOENIX BAKER',
                 date:'19 Jan 2024'
         },
         {
                 title:'Podcast: Creating a better CX Community',
                 link:'#',
-                tags:['tag1','tag2'],
+                tags:['Policy & Regulation','Code & Engineering'],
                 name:'PHOENIX BAKER',
                 date:'19 Jan 2024'
         },
         {
                 title:'Podcast: Creating a better CX Community',
                 link:'#',
-                tags:['tag1','tag2'],
+                tags:['Policy & Regulation','Code & Engineering'],
                 name:'PHOENIX BAKER',
                 date:'19 Jan 2024'
         },
