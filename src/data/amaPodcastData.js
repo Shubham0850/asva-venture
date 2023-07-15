ae8f7e1fb5c13646183486dbcbd3ea9569d8bc27
Shubham0850 <rajshubham0850@gmail.com>
2023-07-15 15:42:16 +0530

    ama section added and tags

---
+++ src/data/amaPodcastData.js
@@ -0,0 +1,34 @@
+export const AmaPodcastData = [
+  {
+    type: "AMA",
+    date: "19 July 2022",
+    title: "Checks and balances: Machine learning and zero-knowledge proofs",
+    authers: ["Elena mines", "Elena mines"],
+    tags: ["Policy & Regulation", "Code & Engineering", "Tech Trends"],
+    twitterLink: "#",
+    spotifyLink: "#",
+    link: "#",
+  },
+
+  {
+    type: "AMA",
+    date: "19 July 2022",
+    title: "Checks and balances: Machine learning and zero-knowledge proofs",
+    authers: ["Elena mines", "Elena mines"],
+    tags: ["Policy & Regulation", "Code & Engineering", "Tech Trends"],
+    twitterLink: "#",
+    spotifyLink: "#",
+    link: "#",
+  },
+
+  {
+    type: "AMA",
+    date: "19 July 2022",
+    title: "Checks and balances: Machine learning and zero-knowledge proofs",
+    authers: ["Elena mines", "Elena mines"],
+    tags: ["Policy & Regulation", "Code & Engineering", "Tech Trends"],
+    twitterLink: "#",
+    spotifyLink: "#",
+    link: "#",
+  },
+];
