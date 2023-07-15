b11c507f88e06ebfde1ef78b08d9db319fcc87e5
Kishor <yoyokunal156@gmail.com>
2023-07-15 16:07:05 +0530

    announcement-latest-file

--- src/components/Announcements/Latest.tsx
+++ src/components/Announcements/Latest.tsx
@@ -1,7 +1,30 @@
+import { Text, Box, Container } from "@chakra-ui/react";
 import React from "react";
+import LatestCard from "../Cards/LatestCard";
 
 function Latest() {
-  return <div>Latest</div>;
+  return (
+    <Container maxW="1300">
+      <Box background={"#fff"}>
+        <Text
+          color="#4C545A"
+          fontSize="24px"
+          fontWeight="700"
+          textTransform="uppercase"
+        >
+          Latest
+        </Text>
+        <LatestCard
+          title={""}
+          link={""}
+          date={""}
+          tags={[]}
+          authers={[]}
+          type={""}
+        />
+      </Box>
+    </Container>
+  );
 }
 
 export default Latest;
