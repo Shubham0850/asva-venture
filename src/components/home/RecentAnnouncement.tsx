96c49d06380801dce9f8f69e63ce86cba3ce250d
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-15 15:03:24 +0530

    done recent announcementcard

--- src/components/home/RecentAnnouncement.tsx
+++ src/components/home/RecentAnnouncement.tsx
@@ -1,8 +1,25 @@
+import { Container,Flex,Text ,Link} from '@chakra-ui/react'
 import React from 'react'
+import { AnnouncementData } from "../../data/announcementData";
+import AnnouncementCard from '../Cards/AnnouncementCard';
 
 function RecentAnnouncement() {
   return (
-	<div>RecentAnnouncement</div>
+	<Container maxW={1300}>
+   <Flex justifyContent={'space-between'}>
+     <Text>Announcements – explore about news</Text>
+     <Link href={'#'}>See All</Link>
+   </Flex>
+
+<Flex  alignItems={'center'}  wrap={'wrap'} gap={6}>
+  {AnnouncementData.map((announcementData) =>{
+       const {title,date,tags,name,link}= announcementData; 
+       return(
+            <AnnouncementCard title={title} date={date} name={name} link={link} tags={tags} />
+       );
+  })}
+</Flex>
+  </Container>
   )
 }
 
