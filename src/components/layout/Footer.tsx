2faa4a06224b0ad1bf183a872d0d27244441ab43
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-17 21:34:29 +0530

    Responsive

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
     <Box backgroundColor={"#1A1A1A"}>
       <Container maxW={1300}>
         <Box>
-          <Flex py={20} justify={"space-between"}>
+          <Flex py={20} direction={{ base: "column", md: "row" }} justify={"space-between"}>
             <Box>
               <Box>
                 <img src="/white-logo.png" alt="asva logo" />
@@ -19,7 +19,8 @@ function Footer() {
                 color={"#FFFFFF"}
                 gap={10}
                 pt={20}
-                direction={{ base: "column", md: "row" }}
+                mb={[20,0]}
+                direction={{ base: "row", md: "row" }}
               >
                 <Link href={"#"}>
                   <Text fontSize={"20px"}>Home</Text>
@@ -36,24 +37,24 @@ function Footer() {
               </Flex>
             </Box>
 
-            <Box>
+            <Box >
               <Box color={"#FFFFFF"}>
                 <Text py={2} fontSize={"20px"}>
                   Contact Us
                 </Text>
                 <Link href={"#"}>
-                  <Text fontSize={20} textDecoration={"underline"}>
+                  <Text fontSize={20} pb={[4,0]} textDecoration={"underline"}>
                     info@asvalabs.com
                   </Text>
                 </Link>
               </Box>
               <Flex
                 color={"#FFFFFF"}
-                justify={{ base: "center", md: "initial" }}
+                justify={{ base: "initial", md: "initial" }}
                 gap={6}
                 style={{ width: "256px", height: "30px" }}
-                direction={{ base: "column", md: "row" }}
-                pt={20}
+                direction={{ base: "row", md: "row" }}
+                pt={[0,20]}
               >
                 <Text fontSize={"20px"}>Follow Us :</Text>
                 <Link href={"#"}>
@@ -96,9 +97,10 @@ function Footer() {
             gap={10}
             p={5}
           >
-            <Text>asvaventures © 2022 All rights reserved</Text>
-            <Link href={"#"}>terms of use</Link>
-            <Link href={"#"}>privacy policy</Link>
+            <Text fontSize={['12px','14px']} letterSpacing={'-1px'} lineHeight={'150%'}> <span className="hidden">asvaventures © </span>2022 All rights reserved</Text>
+            <Text fontSize={['12px','14px']} letterSpacing={'-1px'}> <Link href={"#"} >Terms Of Use</Link></Text>
+            <Text fontSize={['12px','14px']} letterSpacing={'-1px'}> <Link href={"#"}>Privacy Policy</Link></Text>
+           
           </Flex>
         </Box>
       </Container>
