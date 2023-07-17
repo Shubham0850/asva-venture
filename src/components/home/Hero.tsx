e206c4992bb4ba726b90586dcbfafb3eb6d03a6c
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-17 20:50:37 +0530

    responsive

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -8,13 +8,15 @@ function Hero() {
     <Box position={"relative"} bg={"#F8F8F8"} >
       <Container maxW={1300} position={"relative"} zIndex={2}>
       
-          <Box pt={'130px'} pb={'500px'} justifyContent={'flex-start'}>
-            <Box mb={6}>
+          <Box pt={['90px','130px']} pb={['120px','320px']} justifyContent={'flex-start'} >
+           
               <Text
+              
                 width={["100%", "930px"]}
-                fontSize={["26px", "48px"]}
+                fontSize={["36px", "48px"]} 
+                  height={ ["auto", "130px"]}
                 style={{
-                  height: "130px",
+               
                   fontWeight: 700,
                 }}
                 color={"#000"}
@@ -23,31 +25,37 @@ function Hero() {
                 <span style={{ color: "#80FF7D" }}>visionary</span> founders
                 Build on next-generation internet
               </Text>
-            </Box>
-            <Box mb={6}>
+           
+           
               <Text
+              mt={'28px'}
                 width={["100%", "80%"]}
-                fontSize={["12px", "20px"]}
+                fontSize={["14px", "20px"]}
                 style={{
                   height: "92px",
                   fontWeight: 400,
                 }}
-                color={"#000"}
+                color={"#888"}
               >
                 A thesis-driven blockchain-focused venture fund strategically
                 investing in pre-seed and seed companies we lead, co-investments
                 and bring our invaluable expertise to our portfolio projects to
                 grow beyond.
               </Text>
-            </Box>
+           
             <Button
+            gap={2}
+            mt={'50px'}
               backgroundColor="#80FF7D"
               color="#111111"
               padding={["16px", "20px 24px"]}
               fontSize={["12px", "16px"]}
+              rounded={0}
+              margin={''}
             >
               Talk with Us
               <Icon
+              
                 className="blink"
                 as={BsArrowRight}
                 color={"#111111"}
@@ -62,7 +70,7 @@ function Hero() {
         src="/heroBg.png"
         width={"90%"}
         position={"absolute"}
-        top={0}
+        top={[30,0]}
         right={0}
         zIndex={1}
       />
