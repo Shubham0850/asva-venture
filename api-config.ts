ba8d6fcf3e1d5d3575c492c629852260a3149c2f
Shubham0850 <rajshubham0850@gmail.com>
2023-07-31 22:45:35 +0530

    env updated

--- api-config.ts
+++ api-config.ts
@@ -1,2 +1,2 @@
 export const API_ENDPOINT = "https://public-api.wordpress.com/rest/v1.1/sites/";
-export const ENV = "staging-55d8-asvaadmin.wpcomstaging.com";
+export const ENV = "asvaadmin.wpcomstaging.com";
