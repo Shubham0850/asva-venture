fa2cee6220f3e93fd793f4f620d39b26953c7b09
Shubham0850 <rajshubham0850@gmail.com>
2023-08-01 14:04:44 +0530

    env chagnes

--- api-config.ts
+++ api-config.ts
@@ -1,3 +1,3 @@
 export const API_ENDPOINT = "https://public-api.wordpress.com/rest/v1.1/sites/";
-// export const ENV = "asvaadmin.wpcomstaging.com";
-export const ENV = "staging-55d8-asvaadmin.wpcomstaging.com"
+export const ENV = "asvaadmin.wpcomstaging.com";
+// export const ENV = "staging-55d8-asvaadmin.wpcomstaging.com"
